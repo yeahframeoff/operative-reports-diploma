ffb3727187a6e520e8b80f6f3fd92bae22198ab7
yeahframeoff <anthyef@gmail.com>
2016-05-17 08:37:49 +0300

    remove unnecessary stuff

--- operative_reports/urls.py
+++ operative_reports/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from auth import views as auth_views
-from rest_framework.authtoken import views
 
 from .view import hello
 urlpatterns = [
     url(r'^hello', hello),
     url(r'^admin/', admin.site.urls),
-    url(r'^register', auth_views.CreateUserView),
-    url(r'^hello', auth_views.hello),
     url(r'^auth/', include('djoser.urls.authtoken'))
 ]
